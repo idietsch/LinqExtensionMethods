a3b7ab98c987c06e58fdb51fb2ad8bafcd7998ab
Ian Dietsch <wolffox36007@gmail.com>
2020-02-21 15:42:35 -0500

    Add project files.

---
+++ LinqExtensionMethods/Pricing.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExtensionMethods {
+    class Pricing {
+        public string ProductCode { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ LinqExtensionMethods/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExtensionMethods {
+    class Product {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public bool Taxible { get; set; }
+    }
+}
